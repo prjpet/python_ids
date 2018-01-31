1c523e5817bca63a60df8af01273c1c4a131bdfd
Péter Prjevara <pprjevara@desktop-14.students.os3.nl>
2018-01-31 14:14:44 +0100

    statechart and correlation is working.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.5
 #dependencies: pyshark, tshark, wireshark
 
-import os, sys, pyshark, time, hashlib
+import os, sys, pyshark, time, hashlib, math
 from utils import MyParser
 from sniffer import Dissector
 from io_parser import IOParser
@@ -38,7 +38,7 @@ def printDevices(header,devlist):
 def getState(packet_contents,device_list, i):
     func_code = packet_contents["func_code"]
     final_dict = {}
-    print("Packet: ", i, "********** READING ALL DEVICES WITH FUNCTION CODE "+str(func_code)+" - REGISTER DEFAULT. **********")
+    if i < 60: print("Packet: ", i, "********** READING ALL DEVICES WITH FUNCTION CODE "+str(func_code)+" - REGISTER STATE. **********")
     if func_code == 3:
         #since we know that we now read every device's state with func code 6...
         #go through the contents of the holding registers
@@ -48,7 +48,7 @@ def getState(packet_contents,device_list, i):
             device_index = device_index + 1
             #first register default state for device
             if device_index in device_list:
-                device_list[device_index].default_state = device_value
+                device_list[device_index].state = device_value
                 #then add device to the system default state descriptor list
                 if device_list[device_index].logical_group != '': key = int(device_list[device_index].logical_group)
                 else: key = 0
@@ -106,12 +106,14 @@ if __name__ == '__main__':
     previous_packet_contents = ""
     request = False
 
-    mode = [[5,],[300,],[0,]]
+    mode = [[5,],[1200,],[0,]]
     mode_index = 0
 
     mySystemState = SystemState()
 
-
+    TRAINING_DATA = {}
+    TRAINING_STATE_LIST = []
+    LINE_COUNT = 0
     ANOMALY_COUNTER = 0
 
     #printDevices("Parsed devices: ", modbus_device_list)
@@ -163,7 +165,8 @@ if __name__ == '__main__':
                                 previous_system_state = 0
                                 block_time = 0
                                 prev_block_time = 0
-                            #and mySystemState.next_state[1]["digital"] != mySystemState.default_state[1]["digital"]: mode_index += 1
+                                normal_learning_start = time.time()
+                            #and mySystemState.next_state[1]["digital"] != mySystemState.state[1]["digital"]: mode_index += 1
 
                             #we are still in learning phase:
                             #if different states are received, let the operator know that we need to stabilise the system
@@ -172,16 +175,15 @@ if __name__ == '__main__':
                                 if packet_contents["func_code"] == 3:
 
                                     #the getState can be used to run through contents of response for func_code requests
-                                    if mySystemState.default_state == 0:
-                                        mySystemState.default_state = getState(packet_contents,modbus_device_list, i)
-                                        mySystemState.next_state = mySystemState.default_state
+                                    if mySystemState.state == 0:
+                                        mySystemState.state = getState(packet_contents,modbus_device_list, i)
+                                        mySystemState.next_state = mySystemState.state
                                     else:
                                         mySystemState.next_state = getState(packet_contents,modbus_device_list, i)
 
-                                #printDevices("Default state: ", mySystemState.default_state)
+                                #printDevices("Default state: ", mySystemState.state)
 
                         elif mode_index == 1:
-                            normal_learning_start = time.time()
 
                             #BUILD STATECHART FROM DIGITAL STATES
                             #Build digital only list
@@ -201,7 +203,8 @@ if __name__ == '__main__':
                             """
 
                             if packet_contents["func_code"] == 3:
-                                print("********* LEARNING DEFAULTS FINISHED - HASHING DIGITAL STATE.**********")
+                                if current_system_state == 0: print("********* LEARNING DEFAULTS FINISHED - HASHING DIGITAL STATE.**********")
+
                                 current_system_state = getState(packet_contents,modbus_device_list, i)
                                 current_time = time.time()
                                 #see if there is state change
@@ -229,11 +232,6 @@ if __name__ == '__main__':
                                             #increase probability
                                             item_index = mySystemState.digital_statechart[logical_block][hashed_block]["successors"].index(hashed_block)
                                             mySystemState.digital_statechart[logical_block][hashed_block]["prob"][item_index] += 1
-                                        #else:
-                                            #mySystemState.digital_statechart[logical_block][hashed_block]["successors"].append(hashed_block)
-                                            #item_index = mySystemState.digital_statechart[logical_block][hashed_block]["successors"].index(hashed_block)
-                                            #mySystemState.digital_statechart[logical_block][hashed_block]["prob"][item_index] += 1
-
 
                                     else:
                                         mySystemState.digital_statechart[logical_block][hashed_block] = {"successors": [hashed_block,] , "time_delta": [1, ], "prob": [1, ]}
@@ -248,24 +246,53 @@ if __name__ == '__main__':
                                     #Feed all this into LSTM, getting predictions for Td
                                     #Also feed analogue values into LSTM asking for predictions for Analogue values
                                     if previous_system_state != 0:
+                                        current_time_delta = block_time - prev_block_time
                                         if hashed_block not in mySystemState.digital_statechart[logical_block][prev_hashed_block]["successors"]:
                                             mySystemState.digital_statechart[logical_block][prev_hashed_block]["successors"].append(hashed_block)
-                                            mySystemState.digital_statechart[logical_block][prev_hashed_block]["time_delta"].append(block_time - prev_block_time)
+                                            mySystemState.digital_statechart[logical_block][prev_hashed_block]["time_delta"].append(current_time_delta)
                                             mySystemState.digital_statechart[logical_block][prev_hashed_block]["prob"].append(1)
                                         else:
                                             item_index = mySystemState.digital_statechart[logical_block][prev_hashed_block]["successors"].index(hashed_block)
                                             mySystemState.digital_statechart[logical_block][prev_hashed_block]["prob"][item_index] += 1
-                                            mySystemState.digital_statechart[logical_block][prev_hashed_block]["time_delta"][item_index] = block_time - prev_block_time
+                                            mySystemState.digital_statechart[logical_block][prev_hashed_block]["time_delta"][item_index] = current_time_delta
+
+                                        training_line = []
+                                        LINE_COUNT += 1
+                                        if logical_block == 2:
+                                            print(mySystemState.digital_statechart[logical_block])
+                                            print(str(prev_hashed_block) + str(hashed_block))
+                                            current_transition = str(prev_hashed_block) + str(hashed_block)
+                                            if current_transition not in TRAINING_STATE_LIST:
+                                                TRAINING_STATE_LIST.append(current_transition)
 
 
+                                            training_line.append(str(LINE_COUNT))
+                                            training_line.append(str(TRAINING_STATE_LIST.index(current_transition)))
+                                            training_line.append(str(round(current_time_delta, 3)))
+                                            for analogue_device in current_system_state[logical_block]["analogue"]:
+                                                training_line.append(str(analogue_device.state))
+
+                                            if logical_block in TRAINING_DATA:
+                                                TRAINING_DATA[logical_block].append(training_line)
+                                            else:
+                                                TRAINING_DATA[logical_block] = [training_line]
+                                            with open('test_data'+str(logical_block)+'.txt', 'a') as the_file:
+                                                print(', '.join(training_line))
+                                                the_file.write(', '.join(training_line))
+
+
+
+
+
+
+                                #print(mySystemState.digital_statechart)
 
                                 previous_system_state = current_system_state
                                 prev_block_time = block_time
+
+
                                 current_system_state = None
                                 current_time = None
-
-
-                                print(mySystemState.digital_statechart)
                                 #we are in normal learning phase
                                 if next_packet_time - normal_learning_start > mode[mode_index][0]:
                                     mode_index += 1
--- objects.py
+++ objects.py
@@ -3,7 +3,7 @@ class ModbusObject:
     """ a modbus object has the following properties:
     address, digital, current_state, valid_states, tag_name """
 
-    def __init__(self,tag_name="modbus_object", address = 0, digital = True, logical_group = 0, default_state = 0):
+    def __init__(self,tag_name="modbus_object", address = 0, digital = True, logical_group = 0, state = 0):
         """  """
         self.tag_name = tag_name
         self.address = address
@@ -11,7 +11,7 @@ class ModbusObject:
         self.valid_states = []
         self.digital = digital
         self.logical_group = logical_group
-        self.default_state = default_state
+        self.state = state
 
     def __str__(self):
         """ everybody likes printable objects right?
@@ -24,7 +24,7 @@ class ModbusObject:
                 self.valid_states,\
                 self.digital,\
                 self.logical_group,\
-                self.default_state)
+                self.state)
 
 
 class SystemState:
@@ -33,7 +33,7 @@ class SystemState:
 
     def __init__(self):
         """  """
-        self.default_state = 0
+        self.state = 0
         self.next_state = 0
         self.current_state = 0
         self.digital_statechart = { 1:{}, 2:{} }
@@ -50,25 +50,23 @@ class SystemState:
 
     def buildStatechartFromDefault(self):
         string_to_hash = ""
-        for k in self.default_state:
+        for k in self.state:
             #create a hashed state descriptor for the digital states for each of the blocks
             #in the following format:
             #self.digital_statechart = { 1:{}, 2:{} }
             #self.digital_statechart = { 1:{"state1": {"successors": [] , "time_delta": [], "prob": []} }, 2:{} }
             if k in self.digital_statechart:
 
-                for mode in self.default_state[k]:
-                    for element in self.default_state[k]["digital"]:
-                        string_to_hash += str(element)
+                for element in self.state[k]["digital"]:
+                    string_to_hash += str(element)
 
                 self.digital_statechart[k] = { self.hashState(string_to_hash): {"successors": [self.hashState(string_to_hash),] , "time_delta": [1, ], "prob": [1, ]} }
 
             else:
                 self.digital_statechart[k] = {}
 
-                for mode in self.default_state[k]:
-                    for element in self.default_state[k][mode]:
-                        string_to_hash += str(element)
+                for element in self.state[k]["digital"]:
+                    string_to_hash += str(element)
 
                 self.digital_statechart[k] = { self.hashState(string_to_hash): {"successors": [self.hashState(string_to_hash),] , "time_delta": [1, ], "prob": [1, ]} }
 
